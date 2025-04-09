c18357c9848385b88f156e5f9f748bf1df23f3f1
BALM <106144251+BlamzKunG@users.noreply.github.com>
2025-04-09 16:14:59 +0700

    Update stars.lua

--- stars.lua
+++ stars.lua
@@ -1,8 +1,18 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Title of the library", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+local Window = OrionLib:MakeWindow({Name = "BlamzKunG", HidePremium = false,IntroText = "BlamzKunG", SaveConfig = true, ConfigFolder = "OrionTest"})
 
-local Tab = Window:MakeTab({
-	Name = "Tab 1",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
+local Combat = Window:MakeTab({
+    Name = "Combat",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+local Chest = Window:MakeTab({
+    Name = "Chest",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+Combat:AddButton({
+    Name = "Fast Attack"
 })
