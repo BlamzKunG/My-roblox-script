91133435fd85d3cfa50d6381bdb80f4d41d2bf46
BALM <106144251+BlamzKunG@users.noreply.github.com>
2025-04-09 15:59:59 +0700

    Update stars.lua

--- stars.lua
+++ stars.lua
@@ -1,2 +1,8 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Lxxu4k", "DarkTheme")
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Title of the library", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
+local Tab = Window:MakeTab({
+	Name = "Tab 1",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
