e8177cc7dbe2bdeea011e1f8b073bef9368aebf2
BALM <106144251+BlamzKunG@users.noreply.github.com>
2025-04-11 16:27:37 +0700

    Update stars.lua

--- stars.lua
+++ stars.lua
@@ -1,18 +1,31 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "BlamzKunG", HidePremium = false,IntroText = "BlamzKunG", SaveConfig = true, ConfigFolder = "OrionTest"})
 
-local Combat = Window:MakeTab({
-    Name = "Combat",
+local Main = Window:MakeTab({
+    Name = "Main",
     Icon = "rbxassetid://4483345998",
     PremiumOnly = false
 })
 
-local Chest = Window:MakeTab({
-    Name = "Chest",
+local Oher = Window:MakeTab({
+    Name = "Oher",
     Icon = "rbxassetid://4483345998",
     PremiumOnly = false
 })
 
-Combat:AddButton({
-    Name = "Fast Attack"
+Main:AddButton({
+    Name = "troll"
+
+})
+
+Main:AddTextbox({
+    Name = "vtwi;jt",
+    Defualt = "16",
+    TextDisappear = true,
+    Callback = function(Value)
+		local Player = game.Players.localPlayer
+        local chara = Player.Character
+        chara.Humanoid.WalkSpeed = Value
+	end 
+    
 })
