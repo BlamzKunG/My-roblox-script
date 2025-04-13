b922b9424be7690f502d5e20c093f88877bf15bf
BALM <106144251+BlamzKunG@users.noreply.github.com>
2025-04-13 19:56:27 +0700

    Update stars.lua

--- stars.lua
+++ stars.lua
@@ -5,26 +5,26 @@ local Window = Rayfield:CreateWindow({
     LoadingTitle = "Initializing",
     LoadingSubtitle = "Loading UI...",
     ConfigurationSaving = {
-        Enabled = false,
+    Enabled = false,
     }
 })
 
 local Tab = Window:CreateTab("Main")
 
 Tab:CreateToggle({
-    Name = "หัวใหญ่",
-    Callback = function()
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/BlamzKunG/My-roblox-sc/refs/heads/main/HitboxExpand.lua"))()	
+        Name = "หัวใหญ่",
+        Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/BlamzKunG/My-roblox-sc/refs/heads/main/HitboxExpand.lua"))()	
     end,    
 })
 
 Tab:CreateToggle({
-	Name = "ดึงโหด😈😈",
-	CurrentValue = false,
-	Callback = function(Value)
+        Name = "ดึงโหด😈😈",
+        CurrentValue = false,
+        Callback = function(Value)
         loadstring(game:HttpGet("https://raw.githubusercontent.com/BlamzKunG/My-roblox-sc/refs/heads/main/Tpall.lua"))()
-    end,
-        
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/BlamzKunG/My-roblox-sc/refs/heads/main/Hitboxfortpkill.lua"))()
+    end,        
 })
 
 Tab:CreateToggle({
