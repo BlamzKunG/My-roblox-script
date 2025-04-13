2d52ffd2134ad729c256a80a43733c21f33bfe02
BALM <106144251+BlamzKunG@users.noreply.github.com>
2025-04-13 22:33:13 +0700

    Update Hitboxfortpkill.lua

--- Hitboxfortpkill.lua
+++ Hitboxfortpkill.lua
@@ -17,7 +17,7 @@ RunService.Stepped:Connect(function()
                     head.CanCollide = false
                     head.Massless = true
 
-                    -- ป้องกันหัวลอยหลุด --pls
+                    -- ป้องกันหัวลอยหลุด --pls no lag byw
                     local weld = Instance.new("WeldConstraint")
                     weld.Part0 = head
                     weld.Part1 = character:FindFirstChild("UpperTorso") or character:FindFirstChild("Torso") or head
