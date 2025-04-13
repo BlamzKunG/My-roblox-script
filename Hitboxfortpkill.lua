5199e04eedd6ce225fad6e602abc59d60c5ff480
BALM <106144251+BlamzKunG@users.noreply.github.com>
2025-04-13 19:52:25 +0700

    Update Hitboxfortpkill.lua

--- Hitboxfortpkill.lua
+++ Hitboxfortpkill.lua
@@ -9,8 +9,8 @@ RunService.Stepped:Connect(function()
             if character and character:FindFirstChild("Head") then
                 local head = character.Head
 
-                if head.Size ~= Vector3.new(1, 10, 11) then
-                    head.Size = Vector3.new(1, 10, 1)
+                if head.Size ~= Vector3.new(3, 10, 3) then
+                    head.Size = Vector3.new(3, 10, 3)
                     head.Transparency = 0.7 -- มองเห็นราง ๆ
                     head.Color = Color3.fromRGB(255, 0, 0) -- สีแดง เพื่อให้เห็นเด่น
                     head.Material = Enum.Material.Neon -- สะท้อนแสงชัดขึ้น
