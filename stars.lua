54df95b8255da92c873f59f9bc95c21b1cd5bb81
BALM <106144251+BlamzKunG@users.noreply.github.com>
2025-04-13 14:25:49 +0700

    Update stars.lua

--- stars.lua
+++ stars.lua
@@ -11,15 +11,34 @@ local Window = Rayfield:CreateWindow({
 
 local Tab = Window:CreateTab("Main")
 
-Tab:CreateButton({
-	Name = "Click Me",
+Tab:CreateToggle({
+	Name = "หัวใหญ่",
 	Callback = function()
-		print("Hello from Rayfield!")
+        for _, player in pairs(game.Players:GetPlayers()) do
+            if player ~= game.Players.LocalPlayer then
+                local character = player.Character
+                if character and character:FindFirstChild("Head") then
+                    character.Head.Size = Vector3.new(10, 10, 10)
+                    character.Head.Transparency = 1 -- มองไม่เห็น
+                    character.Head.CanCollide = false
+                end
+            end
+        end
+        
+            wait(1) -- ต้องมี wait ไม่งั้นแครช
+        end,
+})
+
+Tab:CreateToggle({
+	Name = "วาร์ปไปยิง",
+	CurrentValue = false,
+	Callback = function(Value)
+		print("God Mode:", Value)
 	end,
 })
 
 Tab:CreateToggle({
-	Name = "God Mode",
+	Name = "Esp",
 	CurrentValue = false,
 	Callback = function(Value)
 		print("God Mode:", Value)
