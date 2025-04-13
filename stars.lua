3ea3506f1380ed567ab5e287ff7eeebec6cd0079
BALM <106144251+BlamzKunG@users.noreply.github.com>
2025-04-13 13:31:56 +0700

    Update stars.lua

--- stars.lua
+++ stars.lua
@@ -1,9 +1,37 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("MyHub", "DarkTheme")
+local Origin = loadstring(game:HttpGet("https://raw.githubusercontent.com/OriginHub/OriginUILibrary/main/Library.lua"))()
 
-local MainTab = Window:NewTab("Main")
-local MainSection = MainTab:NewSection("Controls")
+local Window = Origin:CreateWindow({
+    Name = "My Origin UI", -- ชื่อ UI
+    LoadingTitle = "Loading...", -- ชื่อเวลารัน
+    LoadingSubtitle = "Please wait", -- ข้อความใต้ชื่อ
+    ConfigurationSaving = {
+        Enabled = false, -- ปิดระบบ save config ไว้ก่อน
+    },
+    Discord = {
+        Enabled = false,
+    },
+    KeySystem = false, -- ปิดระบบใส่ Key
+})
 
-MainSection:NewButton("Kill All", "Eliminates all players", function()
-    print("Executing Kill All")
-end)
+-- สร้างแท็บ
+local Tab = Window:CreateTab("Main", 4483362458) -- ชื่อ Tab + รูป icon จาก id
+
+-- สร้าง Section
+local Section = Tab:CreateSection("Main Functions")
+
+-- สร้างปุ่ม
+Section:AddButton({
+    Name = "Click me",
+    Callback = function()
+        print("You clicked the button!")
+    end,
+})
+
+-- สร้าง Toggle
+Section:AddToggle({
+    Name = "Enable Something",
+    Default = false,
+    Callback = function(Value)
+        print("Toggle is now:", Value)
+    end,
+})
