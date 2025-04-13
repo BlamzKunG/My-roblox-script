f1bb1413fd4caed80946854f99f16990267e453a
BALM <106144251+BlamzKunG@users.noreply.github.com>
2025-04-13 16:34:22 +0700

    Update FrozenPlayer.lua

--- FrozenPlayer.lua
+++ FrozenPlayer.lua
@@ -1,1 +1,19 @@
-
+        local RunService = game:GetService("RunService")
+        local LocalPlayer = game.Players.LocalPlayer
+    
+        RunService.RenderStepped:Connect(function()
+            for _, player in pairs(game.Players:GetPlayers()) do
+                if player ~= LocalPlayer then
+                    local character = player.Character
+                    if character and character:FindFirstChild("Head") then
+                        local head = character.Head
+                        head.Size = Vector3.new(1.5, 1.5, 1.5)
+                        head.Transparency = 1
+                        head.CanCollide = false
+    
+                        local mesh = head:FindFirstChildOfClass("SpecialMesh")
+                        if mesh then mesh:Destroy() end
+                    end
+                end
+            end
+        end)
