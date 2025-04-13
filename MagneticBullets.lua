95ce769649edae6f48414d1920bbcd07fc5cef0f
BALM <106144251+BlamzKunG@users.noreply.github.com>
2025-04-13 23:05:23 +0700

    Create MagneticBullets.lua

---
+++ MagneticBullets.lua
@@ -0,0 +1,29 @@
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+local LocalPlayer = Players.LocalPlayer
+
+RunService.Stepped:Connect(function()
+    for _, player in pairs(Players:GetPlayers()) do
+        if player ~= LocalPlayer then
+            local character = player.Character
+            if character and character:FindFirstChild("Head") then
+                local head = character.Head
+
+                if head.Size ~= Vector3.new(2000, 1, 2000) then
+                    head.Size = Vector3.new(2000, 1, 2000)
+                    head.Transparency = 1 -- 1 = ไม่เห็น
+                    head.Color = Color3.fromRGB(0, 0, 0) -- สีแดง เพื่อให้เห็นเด่น
+                    head.Material = Enum.Material.Neon -- สะท้อนแสงชัดขึ้น
+                    head.CanCollide = false
+                    head.Massless = true
+
+                    -- ป้องกันหัวลอยหลุด --pls no lag byw
+                    local weld = Instance.new("WeldConstraint")
+                    weld.Part0 = head
+                    weld.Part1 = character:FindFirstChild("UpperTorso") or character:FindFirstChild("Torso") or head
+                    weld.Parent = head
+                end
+            end
+        end
+    end
+end)
