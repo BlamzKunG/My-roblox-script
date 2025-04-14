151cb9ab3bf890891b1563ebd2e051b14b29cba2
BALM <106144251+BlamzKunG@users.noreply.github.com>
2025-04-14 18:36:23 +0700

    Create AutoFarm.lua

---
+++ AutoFarm.lua
@@ -0,0 +1,22 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+    Name = "เรื้อนดินน้ำมัน(Auto Farm)🥵",
+    LoadingTitle = "🤓☝🏻",
+    LoadingSubtitle = "กำลังดาวน์โหลด...",
+    ConfigurationSaving = {
+    Enabled = true,
+    }
+})
+
+local Tab = Window:CreateTab("Main")
+
+Tab:CreateToggle({
+        Name = "Auto Farm",
+        Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/BlamzKunG/My-roblox-sc/refs/heads/main/Tpkill.lua"))()
+	      loadstring(game:HttpGet("https://raw.githubusercontent.com/BlamzKunG/My-roblox-sc/refs/heads/main/MagneticBullets.lua"))()
+      	loadstring(game:HttpGet("https://raw.githubusercontent.com/BlamzKunG/My-roblox-sc/refs/heads/main/AutoClick.lua"))()
+	      loadstring(game:HttpGet("https://raw.githubusercontent.com/BlamzKunG/My-roblox-sc/refs/heads/main/AutoRejoin.lua"))()
+    end,    
+})
