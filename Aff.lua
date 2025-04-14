1f5648c32e86dbfe3e4fdbab5496765bf23e141e
BALM <106144251+BlamzKunG@users.noreply.github.com>
2025-04-15 00:16:35 +0700

    Update Aff.lua

--- Aff.lua
+++ Aff.lua
@@ -1,3 +1,4 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BlamzKunG/My-roblox-sc/refs/heads/main/Ifpydie.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/BlamzKunG/My-roblox-sc/refs/heads/main/CA-SC-AF.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/BlamzKunG/My-roblox-sc/refs/heads/main/KillForAutoFarm.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/BlamzKunG/My-roblox-sc/refs/heads/main/MagneticBullets.lua"))()
