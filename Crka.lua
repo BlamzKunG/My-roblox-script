183d04bb24a1cb9e5fb8e270eb1a0fc0e1828f73
BALM <106144251+BlamzKunG@users.noreply.github.com>
2025-04-15 16:08:52 +0700

    Update Crka.lua

--- Crka.lua
+++ Crka.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet(""))()
-loadstring(game:HttpGet(""))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BlamzKunG/My-roblox-sc/refs/heads/main/Cr.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BlamzKunG/My-roblox-sc/refs/heads/main/KA.lua"))()
