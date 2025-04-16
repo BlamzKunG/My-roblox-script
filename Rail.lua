221afff58d47cfea78399b53078ecda2fe38658b
BALM <106144251+BlamzKunG@users.noreply.github.com>
2025-04-16 15:10:25 +0700

    Update Rail.lua

--- Rail.lua
+++ Rail.lua
@@ -4,17 +4,16 @@ local window = DrRayLibrary:Load("Lxxuak Hub", "Default")
 local tab = DrRayLibrary.newTab("Main", "ImageIdHere")
 local tab2 = DrRayLibrary.newTab("Auto Farm", "ImageIdHere")
 
-tab.newToggle("Fast Attack", "Fast Attack", false, function(toggleState)
-    getgenv().FastAttack = toggleState
-    if toggleState then
+tab.newToggle("Fast Attack", "Fast Attack", false, function(FastAttack)
+    getgenv().FastAttack = FastAttack
+    if FastAttack then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/BlamzKunG/My-roblox-sc/refs/heads/main/Fat"))()
     end
 end)
 
-tab2.newToggle("Auto Farm", "Auto Farm", false, function(toggleState)
-    if toggleState then
-        print("On")
-    else
-        print("Off")
+tab.newButton("Button", "Prints Hello!", function(AutoFG)
+    getgenv().FastAttack = AutoFG
+    if AutoFG then
+        
     end
 end)
