406744f3fe07fb36b473fb3a15216123eeffeb55
BALM <106144251+BlamzKunG@users.noreply.github.com>
2025-04-16 12:33:51 +0700

    Update Rail.lua

--- Rail.lua
+++ Rail.lua
@@ -1,8 +1,18 @@
 local DrRayLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/DrRay-UI-Library/main/DrRay.lua"))()
 local window = DrRayLibrary:Load("Lxxuak Hub", "Default")
-local tab = DrRayLibrary.newTab("Main", "ImageIdHere")
 
-tab.newToggle("Hitbox Expand", "Head Hitbox Expand", false, function(toggleState)
+local tab = DrRayLibrary.newTab("Main", "ImageIdHere")
+local tab2 = DrRayLibrary.newTab("Esp", "ImageIdHere")
+
+tab.newToggle("Kill All", "Kill All", false, function(toggleState)
+    if toggleState then
+        print("On")
+    else
+        print("Off")
+    end
+end)
+
+tab2.newToggle("Esp", "Esp", false, function(toggleState)
     if toggleState then
         print("On")
     else
