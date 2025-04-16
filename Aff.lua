915e44716ed28b1d7c14506877837b71ddcf0d03
BALM <106144251+BlamzKunG@users.noreply.github.com>
2025-04-16 20:36:20 +0700

    Update Aff.lua

--- Aff.lua
+++ Aff.lua
@@ -1,9 +1,9 @@
 --loadstring(game:HttpGet(""))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/BlamzKunG/My-roblox-sc/refs/heads/main/Ifpydie.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/BlamzKunG/My-roblox-sc/refs/heads/main/CA-SC-AF.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/BlamzKunG/My-roblox-sc/refs/heads/main/KillForAutoFarm.lua"))()
+--loadstring(game:HttpGet("https://raw.githubusercontent.com/BlamzKunG/My-roblox-sc/refs/heads/main/KillForAutoFarm.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/BlamzKunG/My-roblox-sc/refs/heads/main/KA.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/BlamzKunG/My-roblox-sc/refs/heads/main/AutoRejoin.lua"))()
+--loadstring(game:HttpGet("https://raw.githubusercontent.com/BlamzKunG/My-roblox-sc/refs/heads/main/AutoRejoin.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/BlamzKunG/My-roblox-sc/refs/heads/main/AutoRejoin30s.lua"))()
 
 -- กำหนดจุดปลอดภัย (แก้ไข Vector3 ตามจุดที่ต้องการ)
