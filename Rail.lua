75926b8bd897b6ab6b80227227e2d9d0e995b7ac
BALM <106144251+BlamzKunG@users.noreply.github.com>
2025-04-16 09:58:19 +0700

    Update Rail.lua

--- Rail.lua
+++ Rail.lua
@@ -1,12 +1,10 @@
 local Library = loadstring(Game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
-local Window = Library:CreateWindow("PvH Control Panel", "By Blamz")
-local MainTab = Library:CreateTab("Main")
-local ControlSection = MainTab:CreateSection("Controls")
+local Window = Library:NewWindow("Star Rail Simulator")
 
-ControlSection:CreateButton("Click Me", function()
-    print("Button was clicked!")
-end)
+local Tab = Window:NewSection("My Tab")
 
-ControlSection:CreateToggle("God Mode", false, function(state)
-    print("God Mode:", state)
+Tab:CreateButton("Button", function()
+
+print("hello")
+
 end)
