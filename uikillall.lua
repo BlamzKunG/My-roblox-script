796ebd69d3c39d2c14f04a99fcaba72b946afd02
BALM <106144251+BlamzKunG@users.noreply.github.com>
2025-04-16 21:41:34 +0700

    Create uikillall.lua

---
+++ uikillall.lua
@@ -0,0 +1,22 @@
+local DrRayLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/DrRay-UI-Library/main/DrRay.lua"))()
+local window = DrRayLibrary:Load("TikTok:Lxxuak", "Default")
+
+local tab = DrRayLibrary.newTab("Main", "ImageIdHere")
+
+--loadstring(game:HttpGet(""))()
+
+tab.newToggle("Kill All", "โอกาศโดนเตะน้อยมาก", false, function(all)
+    getgenv().FastAttack = all
+    if all then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/BlamzKunG/My-roblox-sc/refs/heads/main/Fat"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/BlamzKunG/My-roblox-sc/refs/heads/main/Cr.lua"))()
+    end
+end)
+
+tab.newToggle("Kill All lnw", "โอกาศโดนเตะสูง", false, function(alll)
+    getgenv().FastAttack = alll
+    if alll then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/BlamzKunG/My-roblox-sc/refs/heads/main/Fat"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/BlamzKunG/My-roblox-sc/refs/heads/main/Cr.lua"))()
+    end
+end)
