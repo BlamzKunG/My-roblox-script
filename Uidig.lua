3bcefd9998b6e54ee0fcc827906610d8b70c1fee
BALM <106144251+BlamzKunG@users.noreply.github.com>
2025-05-01 01:02:24 +0700

    Update Uidig.lua

--- Uidig.lua
+++ Uidig.lua
@@ -6,13 +6,24 @@ local tab2 = DrRayLibrary.newTab("Auto Farm", "ImageIdHere")
 
 --loadstring(game:HttpGet(""))()
 
-tab.newToggle("Fast Attack", "Fast Attack", false, function(FastAttack)
-    getgenv().FastAttack = FastAttack
-    if FastAttack then
-        
+tab.newToggle("Fast Attack", "Fast Attack", false, function(AutoFarm)
+    getgenv().AutoFarm = AutoFarm
+    if AutoFarm then
+        getgenv().AutoFarm = true
+
+task.spawn(function()
+    while getgenv().AutoFarm do
+        pcall(function()
+            game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("GemEvent"):FireServer(1000)
+            game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("DigEvent"):FireServer()
+        end)
+        task.wait() -- แทบจะรัว 60fps
+    end
+end)
     end
 end)
 
 tab.newButton("ขายทั้งหมด", "ขายของในตัวทั้งหมด", function()
-    
+    local hrp = game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart")
+hrp.CFrame = hrp.CFrame * CFrame.new(0, -407, 0)
 end)
