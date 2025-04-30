aea2d63e876657163703376147e4b5d8828e1821
BALM <106144251+BlamzKunG@users.noreply.github.com>
2025-05-01 00:59:17 +0700

    Update Uidig.lua

--- Uidig.lua
+++ Uidig.lua
@@ -1,1 +1,21 @@
+local DrRayLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/DrRay-UI-Library/main/DrRay.lua"))()
+local window = DrRayLibrary:Load("Lxxuak Hub", "Default")
 
+local tab = DrRayLibrary.newTab("Main", "ImageIdHere")
+local tab2 = DrRayLibrary.newTab("Auto Farm", "ImageIdHere")
+
+--loadstring(game:HttpGet(""))()
+
+tab.newToggle("Fast Attack", "Fast Attack", false, function(FastAttack)
+    getgenv().FastAttack = FastAttack
+    if FastAttack then
+        
+    end
+end)
+
+tab.newToggle("Hero Fast Attack", "Hero Fast Attack", false, function(HeroFA)
+    getgenv().HeroFA = HeroFA
+    if HeroFA then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/BlamzKunG/My-roblox-sc/refs/heads/main/HeroFA.lua"))()
+    end
+end)
