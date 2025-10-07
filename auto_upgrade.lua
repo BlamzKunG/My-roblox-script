a13ad5ad6e5eaa076ed9e94e8493db0f6589d769
BALM <106144251+BlamzKunG@users.noreply.github.com>
2025-10-07 11:45:56 +0700

    Create auto_upgrade.lua

---
+++ auto_upgrade.lua
@@ -0,0 +1,46 @@
+local function fireAll()
+    local args = {
+         { 
+            {
+                currencyName = "Grass",
+                amount = "1",
+                max = "7",
+                upgradeValue = "SpawnRate1",
+                cost = "10" 
+            }
+        }
+    }
+    game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Upgrade"):FireServer(unpack(args))
+
+        local args = {
+         { 
+            {
+                currencyName = "Grass",
+                amount = "1",
+                max = "49",
+                upgradeValue = "SpawnRate1",
+                cost = "10" 
+            }
+        }
+    }
+    game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Upgrade"):FireServer(unpack(args))
+
+        local args = {
+         { 
+            {
+                currencyName = "Grass",
+                amount = "1",
+                max = "99",
+                upgradeValue = "SpawnRate1",
+                cost = "10" 
+            }
+        }
+    }
+    game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Upgrade"):FireServer(unpack(args))
+
+end
+
+-- เรียกใช้ทุก 1 วินาที
+while task.wait(0.1) do
+    fireAll()
+end
