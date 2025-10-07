77ae5a83a60589d1d0c5de6d79c155a30e0f6e62
BALM <106144251+BlamzKunG@users.noreply.github.com>
2025-10-07 11:58:51 +0700

    Update auto_upgrade.lua

--- auto_upgrade.lua
+++ auto_upgrade.lua
@@ -52,47 +52,27 @@ game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Incre
 
 
 
-local function fireAll()
-    local args = {
-         { 
-            {
-                currencyName = "Grass",
-                amount = "1",
-                max = "7",
-                upgradeValue = "SpawnRate1",
-                cost = "10" 
-            }
-        }
-    }
-    local args = {
-	    {
-	    	{
-		    	currencyName = "Grass",
-		    	amount = "1",
-		    	max = "49",
-		    	upgradeValue = "GrassAmount",
-		    	cost = "3"
-		    }
-	    }
-    }
-    game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Upgrade"):FireServer(unpack(args))
-
-        local args = {
-         { 
-            {
-                currencyName = "Grass",
-                amount = "1",
-                max = "99",
-                upgradeValue = "GrassValue",
-                cost = "10"
-            }
-        }
-    }
-    game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Upgrade"):FireServer(unpack(args))
-
-end
-
--- เรียกใช้ทุก 1 วินาที
-while task.wait(0.1) do
-    fireAll()
-end
+local args = {
+	{
+		{
+			currencyName = "Grass",
+			amount = "1",
+			max = "49",
+			upgradeValue = "GrassAmount",
+			cost = "3"
+		}
+	}
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Upgrade"):FireServer(unpack(args))
+local args = {
+	{
+		{
+			currencyName = "Grass",
+			amount = "1",
+			max = "99",
+			upgradeValue = "GrassValue",
+			cost = "2"
+		}
+	}
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Upgrade"):FireServer(unpack(args))
